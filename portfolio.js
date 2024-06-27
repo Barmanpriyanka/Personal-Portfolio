b6d069a2fb810402fad5be89212d317514154bfa
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-06-27 17:04:54 +0530

    icons

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-<script src="https://kit.fontawesome.com/f6427b8065.js" crossorigin="anonymous"></script>
+
 var Typed=new Typed(".text",{
     strings :["Student","Frontend developer","web Developer"],
     typespeed:100,
