df8ae36397d0406f01860197cbb3a7a9a9ca0f5c
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-06-28 00:43:41 +0530

    changes icons

--- portfolio.js
+++ portfolio.js
@@ -1,37 +1,31 @@
-var Typed=new Typed(".text",{
-    strings :["Student","Frontend developer","web Developer"],
-    typespeed:100,
-    backspeed:100,
-    backDelay:1000,
-    loop:true
-
+// Typed.js initialization
+var typed = new Typed(".text", {
+    strings: ["Student", "Frontend Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
 });
+
 // Tab navigation
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
 
-function opentab(event, tabname) {
-    for (var tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for (var tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
+for (var i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].addEventListener("click", function(event) {
+        for (var j = 0; j < tabLinks.length; j++) {
+            tabLinks[j].classList.remove("active-link");
+        }
+        for (var k = 0; k < tabContents.length; k++) {
+            tabContents[k].classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        var tabName = event.currentTarget.getAttribute("data-tab");
+        document.getElementById(tabName).classList.add("active-tab");
+    });
 }
 
-// Menu functions
-var sidemenu = document.getElementById("sidemenu");
-function openmenu() {
-    sidemenu.style.right = "0";
-}
-function closemenu() {
-    sidemenu.style.right = "-200px";
-}
-
-// Form submission
+// Form submission to Google Sheets
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxvdw-sfhMzdJXrOG2X9vT6y7jVGeE2P7S4BMbkz0x1cR1WCzy5BsHnl30IE54LcQa_/exec';
 const form = document.forms['submit-to-google-sheet'];
 const msg = document.getElementById("msg");
@@ -40,10 +34,11 @@ form.addEventListener('submit', e => {
     e.preventDefault();
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
             msg.innerHTML = "Message sent successfully";
-            setTimeout(() => {
-                msg.innerHTML = "";
-            }, 5000);
+            setTimeout(() => msg.innerHTML = "", 5000);
             form.reset();
         })
         .catch(error => {
