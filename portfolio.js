f80592cbcc47d81b3112eed8e8f0316f47833413
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-06-27 21:44:40 +0530

    modified   portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,3 @@
-
 var Typed=new Typed(".text",{
     strings :["Student","Frontend developer","web Developer"],
     typespeed:100,
@@ -6,4 +5,49 @@ var Typed=new Typed(".text",{
     backDelay:1000,
     loop:true
 
+});
+// Tab navigation
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(event, tabname) {
+    for (var tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (var tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Menu functions
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+// Form submission
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxvdw-sfhMzdJXrOG2X9vT6y7jVGeE2P7S4BMbkz0x1cR1WCzy5BsHnl30IE54LcQa_/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully";
+            setTimeout(() => {
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        })
+        .catch(error => {
+            console.error('Error!', error.message);
+            msg.innerHTML = "Error sending message";
+        });
 });
